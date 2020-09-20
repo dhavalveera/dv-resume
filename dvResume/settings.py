c7596798d99fa9ff4069b640fd1256b730c2c149
Dhaval Vira <dhavalveera@gmail.com>
2020-09-20 20:35:32 +0530

    Dhaval Vira Resume/Portfolio

--- dvResume/settings.py
+++ dvResume/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import smtplib
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,11 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #Custom App
-    'dv_resume',
-
     #Third-Party Apps
     'gunicorn',
+
+    #Custom App
+    'dv_resume',
 ]
 
 MIDDLEWARE = [
